ff54ab5400d52de3e15a43fbe8c01a62b751f2f0
minhncedutw <minhnc.edu.tw@gmail.com>
2019-05-11 11:13:38 +0800

    myself util libs

---
+++ python_tips/GeneralUtils.py
@@ -0,0 +1,194 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+''' Description
+'''
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+__author__ = "CONG-MINH NGUYEN"
+__copyright__ = "Copyright (C) 2019, HANDBOOK"
+__credits__ = ["CONG-MINH NGUYEN"]
+__license__ = "GPL"
+__version__ = "1.0.1"
+__date__ = "5/10/2019"
+__maintainer__ = "CONG-MINH NGUYEN"
+__email__ = "minhnc.edu.tw@gmail.com"
+__status__ = "Development"  # ["Prototype", "Development", or "Production"]
+# Project Style: https://dev.to/codemouse92/dead-simple-python-project-structure-and-imports-38c6
+# Code Style: http://web.archive.org/web/20111010053227/http://jaynes.colorado.edu/PythonGuidelines.html#module_formatting
+
+#==============================================================================
+# Imported Modules
+#==============================================================================
+import argparse
+from pathlib import Path
+import os.path
+import sys
+import time
+
+os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
+os.environ["CUDA_VISIBLE_DEVICES"] = ""  # The GPU id to use, usually either "0" or "1"
+
+import numpy as np
+
+from typing import Union, Any, List, Optional
+# Source: https://docs.python.org/3/library/typing.html
+# Source: https://mypy.readthedocs.io/en/latest/cheat_sheet_py3.html
+
+#==============================================================================
+# Constant Definitions
+#==============================================================================
+
+#==============================================================================
+# Function Definitions
+#==============================================================================
+'''**************************************************************
+Python's basic lib
+'''
+
+# Source: https://docs.python.org/3/library/pathlib.html#pathlib.Path.cwd
+def str2path(string: str) -> Path:
+    return Path(string)
+
+
+def path2str(path: Path) -> str:
+    if isinstance(path, Path): string = str(path.resolve())
+    else: string = path
+    return string
+
+
+def get_cur_file_path() -> Path:
+    """
+    Description: get path of current file
+    :param NAME: TYPE, MEAN
+    :return: TYPE, MEAN
+    """
+    return Path(__file__).resolve()
+
+
+def get_cur_parent_dir() -> Path:
+    """
+    Description: get parent directory of current file
+    :param NAME: TYPE, MEAN
+    :return: TYPE, MEAN
+    """
+    return Path(__file__).resolve().parent
+
+
+def get_cur_exe_dir() -> Path:
+    """
+    Description: get current execution directory
+    :param NAME: TYPE, MEAN
+    :return: TYPE, MEAN
+    """
+    return Path(__file__).cwd()
+
+
+def makedir(path: Union[str, Path], mode=0o777, parents: bool=True, exist_ok: bool=False, verbose=False):
+    """
+    Description:
+    :param path: [Path, str], path
+    :param mode: [0o777, 0o444, ...], chmod(refer: https://help.ubuntu.com/community/FilePermissions)
+    :param parents: [Path, str], True: if path is relative & False: if path is absolute
+    :param exist_ok: boolean, if path already exists, True: force overwrite & False: raise error
+    :return: TYPE, MEAN
+    """
+    if verbose:
+        print('Create directory: \n\t{} \n\t{}'.format(Path(path).resolve(), Path(path).absolute()))
+    Path(path).mkdir(mode=mode, parents=parents, exist_ok=exist_ok)
+
+
+'''**************************************************************
+Numpy lib
+'''
+def onehot_encoding(labels, n_classes) -> np.ndarray:
+    """
+    Description: convert integer labels to one hot
+    :param labels: an [int, ndarray], a label array of shape (d0, d1, d2, ...dn)
+    :param n_classes: an int, number of classes
+    :return: [ndarray], an one hot array of shape (d0, d1, ...dn, n_classes)
+    """
+    onehot = np.identity(n_classes)[labels]
+    return onehot
+
+
+def onehot_decoding(probs, class_axis) -> np.ndarray:
+    """
+    Description: convert one-hot encoding to labels
+    :param probs: [ndarray], an probability array, one-hot-encoding type of shape (d0, d1, ...dn)
+    :param class_axis: int, axis of classes in 'probs' array(0 <= class_axis <= n)
+    :return: [int, ndarray], an label array of shape (d0, d1, ...dn-1)
+    """
+    labels = np.argmax(np.asarray(probs), axis=class_axis)
+    return labels
+
+
+def rand_int(low: int, high: int) -> int:
+    return np.random.random_integers(low=low, high=high)
+
+def rand_int_arr(low: int, high: int, size: List[np.uint]) -> np.ndarray:
+    return np.random.random_integers(low=low, high=high, size=size)
+
+def rand_float_arr(size: List[np.uint]) -> np.ndarray:
+    return np.random.random(size=size)
+
+
+def sample_indices(n_samples: int, max_index: int, replace: bool=None) -> np.ndarray:
+    """
+    Get a list indice sample for an array
+    :param num_samples: an integer, number of expected samples
+    :param length: an integer, length of array
+    :return: an array of numpy, is a list of indices
+    """
+    if replace is None:
+        replace = n_samples > max_index
+    new_indices = np.random.choice(a=max_index, size=n_samples, replace=replace)
+    return new_indices
+
+
+def sample_arrays(arrs: List[np.ndarray], num_samples: int) -> List[np.ndarray]:
+    """
+    Sample a list of arrays
+    :param num_samples: an integer, number of expected samples
+    :param return_indices: a boolean, that you want to return indices with your sampled arrays or not
+    :param **kwargs: a list of arguments, is a list of arrays that you want to synchronically sample(they must have the same length)
+    :return: a list of numpy array, that are synchronically-sampled arrays(include indices if toggle on 'return_indices')
+    """
+    lengths = [len(arr) for arr in arrs]
+    assert len(np.unique(lengths)) == 1, "Input arguments must have SAME length!"
+
+    # random selecting sample indices
+    new_indices = sample_indices(n_samples=num_samples, max_index=lengths[0])
+
+    # shuffle all arrays by the same order
+    results = [arr[new_indices] for arr in arrs]
+
+    return results
+
+def stack_list_vertical(arrs: List[np.ndarray]):
+    return np.vstack(arrs)
+
+def stack_list_horizontal(arrs: List[np.ndarray]):
+    return np.hstack(arrs)
+
+#==============================================================================
+# Main function
+#==============================================================================
+def _main_(args):
+    print('Hello World! This is {:s}'.format(args.desc))
+
+    # config_path = args.conf
+    # with open(config_path) as config_buffer:    
+    #     config = json.loads(config_buffer.read())
+    path = 'gdgergegd'
+    makedir(path=path, exist_ok=True, verbose=True)
+
+
+if __name__ == '__main__':
+    argparser = argparse.ArgumentParser(description='Your program name!!!')
+    argparser.add_argument('-d', '--desc', help='description of the program', default='HANDBOOK')
+    # argparser.add_argument('-c', '--conf', default='config.json', help='path to configuration file')
+
+    args = argparser.parse_args()
+    _main_(args)
---
+++ python_tips/PointCloudUtils.py
@@ -0,0 +1,170 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+''' Description
+'''
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+__author__ = "CONG-MINH NGUYEN"
+__copyright__ = "Copyright (C) 2019, HANDBOOK"
+__credits__ = ["CONG-MINH NGUYEN"]
+__license__ = "GPL"
+__version__ = "1.0.1"
+__date__ = "5/10/2019"
+__maintainer__ = "CONG-MINH NGUYEN"
+__email__ = "minhnc.edu.tw@gmail.com"
+__status__ = "Development"  # ["Prototype", "Development", or "Production"]
+# Project Style: https://dev.to/codemouse92/dead-simple-python-project-structure-and-imports-38c6
+# Code Style: http://web.archive.org/web/20111010053227/http://jaynes.colorado.edu/PythonGuidelines.html#module_formatting
+
+#==============================================================================
+# Imported Modules
+#==============================================================================
+import argparse
+from pathlib import Path
+import os.path
+import sys
+import time
+
+os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
+os.environ["CUDA_VISIBLE_DEVICES"] = ""  # The GPU id to use, usually either "0" or "1"
+
+import numpy as np
+import open3d as op3
+from GeneralUtils import Union, path2str, stack_list_horizontal, sample_arrays
+
+#==============================================================================
+# Constant Definitions
+#==============================================================================
+
+#==============================================================================
+# Function Definitions
+#==============================================================================
+def label_to_color(labels: np.ndarray) -> np.ndarray:
+    """
+    Convert labels to colors(label's values must be in range [1, 6])
+    :param labels: 1D ndarray, list of labels
+    :return: ndarray of color codes
+    """
+    map_label_to_rgb = {
+        1: [0, 255, 0], # green
+        2: [0, 0, 255], # blue
+        3: [255, 0, 0], # red
+        4: [255, 0, 255],  # purple
+        5: [0, 255, 255],  # cyan
+        6: [255, 255, 0],  # yellow
+    }
+    colors = np.array([map_label_to_rgb[label] for label in labels])
+    return colors
+
+def pc_to_points(point_cloud: op3.PointCloud) -> np.ndarray:
+    """
+    Convert point cloud of Open3D object to numpy array
+    :param point_cloud_object: a Open3D object, of point cloud
+    :return: a numpy array of points
+    """
+    coords = np.asarray(point_cloud.points)
+    colors = np.asarray(point_cloud.colors)
+    return stack_list_horizontal((coords, colors))
+
+def points_to_pc(points: np.ndarray) -> op3.PointCloud:
+    """
+    Convert point cloud of numpy array to Open3D object
+    :param points: a numpy array of points, that describes the point cloud
+    :return: a point cloud object of Open3D
+    """
+    if isinstance(points, op3.PointCloud): point_cloud = points
+    else:
+        point_cloud = op3.PointCloud()
+        point_cloud.points = op3.Vector3dVector(points[:, :3])
+        if points.shape[1] > 5:
+            point_cloud.colors = op3.Vector3dVector(points[:, 3:6])
+    return point_cloud
+
+def coords_colors_to_points(coords: np.ndarray, colors: np.ndarray) -> np.ndarray:
+    return stack_list_horizontal(arrs=(coords, colors))
+
+def coords_colors_to_pc(coords: np.ndarray, colors: np.ndarray) -> op3.PointCloud:
+    return points_to_pc(points=coords_colors_to_points(coords=coords, colors=colors))
+
+def coords_labels_to_points(coords: np.ndarray, labels: np.ndarray) -> np.ndarray:
+    colors = label_to_color(labels=labels)
+    return coords_colors_to_points(coords=coords, colors=colors)
+
+def coords_labels_to_pc(coords: np.ndarray, labels: np.ndarray) -> op3.PointCloud:
+    colors = label_to_color(labels=labels)
+    points = coords_colors_to_points(coords=coords, colors=colors)
+    return points_to_pc(points=points)
+
+
+def load_ply_as_pc(file_path: Union[str, Path]) -> op3.PointCloud:
+    """
+    Load a point cloud from a .ply file
+    :param file_path: a string, path to the .ply file
+    :return: a point cloud object of Open3D
+    """
+    point_cloud = op3.read_point_cloud(filename=path2str(file_path))
+    return point_cloud
+
+def load_ply_as_points(file_path: Union[str, Path]) -> np.ndarray:
+    """
+    Load a point cloud from a .ply file
+    :param file_path: a string, path to the .ply file
+    :return: an numpy array of points
+    """
+    point_cloud = load_ply_as_pc(file_path=path2str(file_path))
+    return pc_to_points(point_cloud)
+
+def measure_pc_centroid(point_cloud: Union[np.ndarray, op3.PointCloud]):
+    """
+    Mesure the centroid of point cloud
+    :return: a point as an array(each element in array is x, y, z, ...)
+    """
+    if isinstance(point_cloud, op3.PointCloud):
+        points = pc_to_points(point_cloud)
+    centroid = np.mean(points, axis=0)
+    return centroid
+
+def sample_pc(point_cloud: op3.PointCloud, n_samples:int) -> op3.PointCloud:
+    """
+    Sample the point cloud
+    :param num_samples: an integer, that is the number of points you want to sample
+    """
+    points = pc_to_points(point_cloud=point_cloud)
+    points = sample_arrays(arrs=(points), n_samples=n_samples)
+    return points_to_pc(points)
+
+def visualize_pc(*args):
+    """
+    Visualize of list of point clouds
+    :param *args: a list of Open3D objects, that are point clouds
+    """
+    point_clouds = [points_to_pc(obj) for obj in args]
+    op3.draw_geometries([*point_clouds])
+
+#==============================================================================
+# Main function
+#==============================================================================
+def _main_(args):
+    print('Hello World! This is {:s}'.format(args.desc))
+
+    # config_path = args.conf
+    # with open(config_path) as config_buffer:    
+    #     config = json.loads(config_buffer.read())
+    pipe_path1 = 'E:/CLOUD/GDrive(t105ag8409)/Projects/ARLab/Point Cloud Robot Grasping/camera/orbbec_astra_s/recorded_data/00094.ply'
+    pipe_path2 = 'E:/CLOUD/GDrive(t105ag8409)/Projects/ARLab/Point Cloud Robot Grasping/camera/orbbec_astra_s/recorded_data/00100.ply'
+    point_cloud1 = load_ply_as_pc(file_path=pipe_path1)
+    points1 = load_ply_as_points(file_path=pipe_path1)
+    point_cloud2 = load_ply_as_pc(file_path=pipe_path2)
+    visualize_pc(point_cloud2, point_cloud1)
+    visualize_pc(point_cloud2, coords_labels_to_points(coords=points1[:, :3], labels=np.ones(len(points1))))
+
+
+if __name__ == '__main__':
+    argparser = argparse.ArgumentParser(description='Your program name!!!')
+    argparser.add_argument('-d', '--desc', help='description of the program', default='HANDBOOK')
+    # argparser.add_argument('-c', '--conf', default='config.json', help='path to configuration file')
+
+    args = argparser.parse_args()
+    _main_(args)
