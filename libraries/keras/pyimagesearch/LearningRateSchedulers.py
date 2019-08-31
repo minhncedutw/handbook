8e01edb4e758a78e1556b7b99b33e80dcc14bc41
minhncedutw <minhnc.edu.tw@gmail.com>
2019-08-31 15:14:41 +0800

    add: keras learning rate schedule

---
+++ libraries/keras/pyimagesearch/LearningRateSchedulers.py
@@ -0,0 +1,111 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+''' Description
+'''
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+__author__      = "CONG-MINH NGUYEN"
+__copyright__   = "Copyright (C) ${YEAR}, ${PROJECT_NAME}"
+__credits__     = ["CONG-MINH NGUYEN"]
+__license__     = "GPL"
+__version__     = "1.0.1"
+__date__        = "${DATE}"
+__maintainer__  = "CONG-MINH NGUYEN"
+__email__       = "minhnc.edu.tw@gmail.com"
+__status__      = "Development" # ["Prototype", "Development", or "Production"]
+# Project Style: https://dev.to/codemouse92/dead-simple-python-project-structure-and-imports-38c6
+# Code Style: http://web.archive.org/web/20111010053227/http://jaynes.colorado.edu/PythonGuidelines.html#module_formatting
+
+#==============================================================================
+# Imported Modules
+#==============================================================================
+import argparse
+import os.path
+import sys
+import time
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+#==============================================================================
+# Constant Definitions
+#==============================================================================
+
+#==============================================================================
+# Function Definitions
+#==============================================================================
+class LearningRateDecay:
+    def plot(self, epochs, title="Learning Rate Schedule"):
+        # compute the set of learning rates for each corresponding
+        # epoch
+        lrs = [self(i) for i in range(epochs)]
+
+        plt.style.use("ggplot")
+        plt.figure()
+        plt.plot(range(epochs), lrs)
+        plt.title(title)
+        plt.xlabel("Epoch #")
+        plt.ylabel("Learning Rate")
+
+
+class StepDecay(LearningRateDecay):
+    def __init__(self, initAlpha=0.01, factor=0.25, dropEvery=10):
+        # store the base initial learning rate, drop factor, and
+        # epochs to drop every
+        self.initAlpha = initAlpha
+        self.factor = factor
+        self.dropEvery = dropEvery
+
+    def __call__(self, epoch):
+        # compute the learning rate for the current epoch
+        exp = np.floor((1 + epoch) / self.dropEvery)
+        alpha = self.initAlpha * (self.factor ** exp)
+
+        # return the learning rate
+        return float(alpha)
+
+
+class PolynomialDecay(LearningRateDecay):
+    def __init__(self, maxEpochs=100, initAlpha=0.01, power=1.0):
+        # store the maximum number of epochs, base learning rate,
+        # and power of the polynomial
+        self.maxEpochs = maxEpochs
+        self.initAlpha = initAlpha
+        self.power = power
+
+    def __call__(self, epoch):
+        # compute the new learning rate based on polynomial decay
+        decay = (1 - (epoch / float(self.maxEpochs))) ** self.power
+        alpha = self.initAlpha * decay
+
+        # return the new learning rate
+        return float(alpha)
+
+#==============================================================================
+# Main function
+#==============================================================================
+def _main_(args):
+    print('Hello World! This is {:s}'.format(args.desc))
+
+    sd = StepDecay(initAlpha=0.01, factor=0.5, dropEvery=10)
+    sd.plot(epochs=100, title="Step-based Decay")
+    plt.show()
+
+    ld = PolynomialDecay(maxEpochs=100, initAlpha=0.01, power=1)
+    ld.plot(epochs=100, title="Linear Decay")
+    plt.show()
+
+    pd = PolynomialDecay(maxEpochs=100, initAlpha=0.01, power=5)
+    pd.plot(epochs=100, title="Polynomial Decay (p=5)")
+    plt.show()
+
+
+if __name__ == "__main__":
+    argparser = argparse.ArgumentParser(description='Your program name!!!')
+    argparser.add_argument('-d', '--desc', help='description of the program', default='${PROJECT_NAME}')
+    # argparser.add_argument('-c', '--conf', default='config.json', help='path to configuration file')
+
+    args = argparser.parse_args()
+    _main_(args)
